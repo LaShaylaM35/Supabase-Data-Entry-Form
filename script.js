624529b75014ed0eb721660589918954d92d743c
LaShaylaM35 <lashaylam@yahoo.com>
2026-01-12 20:12:18 -0600

    making updates

---
+++ script.js
@@ -0,0 +1,36 @@
+let url = "https://nbwqtuymbktwrhmopjti.supabase.co/rest/v1/UserData";
+let apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5id3F0dXltYmt0d3JobW9wanRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MTA3MDcsImV4cCI6MjA4MDM4NjcwN30.V6AO7DgIWRYIn8sHdWDjGVLQ0PSHAsl_6vyGKk0z2Qs";
+//let urlProducts = "https://nbwqtuymbktwrhmopjti.supabase.co/rest/v1/Product";
+
+
+
+
+
+async function createLead(){
+
+    event.preventDefault();
+
+    let name = document.getElementById("contactName").value;
+    let email = document.getElementById("contactEmail").value;
+    let message = document.getElementById("contactMessage").value;
+
+    let lead = {
+        "name" : name , 
+        "email" : email,
+        "message" : message
+    }
+
+ 
+    let response = await fetch(url , {
+        method: "POST",
+        headers: {
+            "apikey" : apikey,
+            "Content-Type" : "application/json"
+        }, 
+        body : JSON.stringify(lead)
+    })
+
+    console.log("Successful Submission");
+    console.log(lead);
+
+}
